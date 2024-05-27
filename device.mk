5c89ab03c11bd812ff063bfb65f3a1aa6310d01f
adnotfound <adnotfound@gmail.com>
2024-05-27 20:09:15 +0800

    build vendor.lge.hardware.audio.dac.control@2.0-service.proprietary

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
 # DAC
 PRODUCT_PACKAGES += \
     QuadDACPanel \
-    vendor.lge.hardware.audio.dac.control@2.0-service
+    vendor.lge.hardware.audio.dac.control@2.0-service.proprietary
 
 # Dalvik/HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
